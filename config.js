c792e093668c231016ae9059f516cd273ec046be
Milos <miloscpt@gmail.com>
2016-03-18 15:33:22 +0100

    Added user model, started building api

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-		"database" : "",
+		"database" : "mongodb://root:root@ds011429.mlab.com:11429/userstory-crni",
 		"port" : process.env.PORT || 3000,
 		"secretKey" : "Kljuc"
 };
--- server.js
+++ server.js
@@ -2,6 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var config = require('./config.js');
+var mongoose = require('mongoose');
+
+mongoose.connect(config.database, function(err){
+	if(err){
+		console.log(err);
+	} else {
+		console.log('Connected to the database!');
+	}
+});
 
 var app = express();
 
@@ -9,6 +18,11 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(morgan("combined"));
+
+
+var api = require('./app/routes/api')(app, express);
+app.use('/api', api);
+
 
 app.get('*', function(req, res){
 	res.sendFile(__dirname + '/public/views/index.html');
