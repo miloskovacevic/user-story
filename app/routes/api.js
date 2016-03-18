c4bfd824409768f5d78606b4a2d9c282a4e98be8
Milos <miloscpt@gmail.com>
2016-03-18 18:26:03 +0100

    Login route done

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,6 +4,23 @@ var config = require('./../../config');
 
 var secretKey = config.secretKey;
 
+var jsonwebtoken = require('jsonwebtoken');
+
+
+function createToken(user){
+	var token = jsonwebtoken.sign({
+		_id:      user._id,
+		name:     user.name,
+		username: user.username
+
+	}, secretKey, {
+		expiresInMinute: 1440
+	});
+
+
+	return token;
+}
+
 
 
 module.exports = function(app, express){
@@ -27,6 +44,48 @@ module.exports = function(app, express){
 			res.json({message: 'User has been created!'});
 		});
 	});
+
+
+	api.get('/users', function(req, res){
+		User.find({}, function(err, users){
+			if(err){
+				res.send(err);
+				return;
+			}
+
+			res.json(users);
+		});
+	});
+
+
+	api.post('/login', function(req, res){
+		User.findOne({ 
+			username: req.body.username
+		}).select('password').exec(function(err, user){
+			if(err) throw err;
+
+			if(!user){
+				res.send({ message: 'User doesnt exist!'});
+			} else if(user) {
+				var validPassword = user.comparePassword(req.body.password);
+
+				if(!validPassword){
+					res.send({message: 'Invalid Password'});
+				} else {
+					//token
+					var token = createToken(user);
+
+					res.json({
+						success: true,
+						message: 'Successfuly logged in!',
+						token:   token
+					});
+
+				}
+			}
+		});
+	});
+
 
 	return api;
 }
