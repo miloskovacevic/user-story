11cb382b6ab01eff2fc4a6ec3b16110b90e7da55
Milos <miloscpt@gmail.com>
2016-03-19 20:04:56 +0100

    Backend done

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,4 +1,5 @@
 var User = require('./../models/user');
+var Story = require('./../models/story');
 
 var config = require('./../../config');
 
@@ -9,7 +10,7 @@ var jsonwebtoken = require('jsonwebtoken');
 
 function createToken(user){
 	var token = jsonwebtoken.sign({
-		_id:      user._id,
+		id:      user._id,
 		name:     user.name,
 		username: user.username
 
@@ -107,13 +108,38 @@ module.exports = function(app, express){
 		}
 	});
 
-	
-	api.get('/', function(req, res){
-		res.json('Hello World!');
+	api.route('/')
+		.post(function(req, res){
+			var story = new Story({
+				creator: req.decoded.id,
+				content: req.body.content
+			});
 
-	})
+			story.save(function(err){
+				if(err) {
+					res.send(err);
+					return;
+				}
+
+				res.json({message: 'New Story Created!'});
+			});
+		})
+
+		.get(function(req, res){
+			Story.find({ creator: req.decoded.id}, function(err, stories){
+				if(err){
+					res.send(err);
+					return;
+				}
+
+				res.json(stories);
+			});		
+	});
 
 
+	api.get('/me', function(req, res){
+		res.json(req.decoded)
+	});
 
 	return api;
 }
